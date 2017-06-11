0cbee9700bb989a792a808f018bcc998d3702c74
Sagar Devkota <sagarda7@yahoo.com>
2017-06-11 17:45:08 +0545

    API added with Distance matrix

--- src/pages/routedetail/routedetail.ts
+++ src/pages/routedetail/routedetail.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Constants } from '../../services/constants';
+import { Http } from '@angular/http';
 
 /**
  * Generated class for the RoutedetailPage page.
@@ -21,34 +23,42 @@ export class RoutedetailPage {
   startLocation:any;
   endAddress: any;
   endLocation:any;
+  placesETARows:any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+constructor(public navCtrl: NavController, public navParams: NavParams, public constants:Constants, public http:Http) {
     this.route=navParams.get("data");
     this.startAddress = this.navParams.get("startAddress");
     this.endAddress = this.navParams.get("endAddress");
     this.startLocation = this.navParams.get("startLocation");
     this.endLocation = this.navParams.get("endLocation");
-
-
-
-    //console.log(this.route);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RoutedetailPage');
-    this.origins.push(this.startLocation.lat+","+this.startLocation.lng);
+    //this.origins.push(this.startLocation.lat+","+this.startLocation.lng);
+
+    this.origins.push("3.218561,101.564353");
     let i=0;
     for(let trip of this.route.trips){
       this.destinations.push(trip.stops[0].stopLat+","+trip.stops[0].stopLon);
       this.origins.push(trip.stops[trip.stops.length-1].stopLat+","+trip.stops[trip.stops.length-1].stopLon);
       i++;
     }
+    //this.destinations.push(this.endLocation.lat+","+this.endLocation.lng);
+    this.destinations.push("3.219405,101.593238");
 
-    this.destinations.push(this.endLocation.lat+","+this.endLocation.lng);
+    let url=this.constants.getMatrixURL(this.origins.join("|"),this.destinations.join("|"));
 
-    console.log(this.origins);
-    console.log("=======");
-    console.log(this.destinations);
+    this.http.get(url).subscribe(data => {
+        let body = data.json();
+        //console.log(body);
+        this.placesETARows=body.rows;
+        console.log(this.placesETARows);
+
+    });
+
+    //console.log(url);
+
   }
 
   goBackClicked(){
