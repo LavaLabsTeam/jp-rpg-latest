0c03eae5dcf609a56aa2b4a7ea551f8fa60e86dc
Tharindu Gamage <tharindu@lavalabs.net>
2018-10-01 11:00:32 +0800

    working on google place search changes

--- src/pages/placesearch/placesearch.ts
+++ src/pages/placesearch/placesearch.ts
@@ -2,6 +2,7 @@ import { Http } from '@angular/http';
 import { Component } from '@angular/core';
 import { IonicPage, NavParams } from 'ionic-angular';
 import { ViewController } from 'ionic-angular';
+import { Constants } from '../../services/constants';
 declare var google:any;
 /**
  * Generated class for the PlacesearchPage page.
@@ -18,7 +19,8 @@ export class PlacesearchPage {
   search: string;
   name:string;
   geocoder:any;
-  constructor(public viewCtrl: ViewController,public navParams: NavParams, private http:Http) {
+  items: any;
+  constructor(public viewCtrl: ViewController,public navParams: NavParams, private http:Http, public constants:Constants) {
     //alert(this.navParams.data.name);
     // if(this.navParams.data.name=="start"){
     //   this.search="Jetty, Pengalan Weld Georgetown George Town Penang Malaysia";
@@ -28,7 +30,7 @@ export class PlacesearchPage {
     //   this.search="Komtar George Town Penang Malaysia";
     // }
 
-    this.geocoder = new google.maps.Geocoder();
+    // this.geocoder = new google.maps.Geocoder();
   }
 
   ionViewDidLoad() {
@@ -41,24 +43,31 @@ export class PlacesearchPage {
       this.viewCtrl.dismiss();
   }
 
-  getAddressOnChange(place){
-    console.log(place.geometry);
-    var obj=this;
-    if(place.geometry===undefined){
-      this.geocoder.geocode( { 'address': this.search}, function(results, status) {
-        if (status == 'OK') {
-          results[0]['name']=obj.search;
-          obj.viewCtrl.dismiss({place:results[0]});
-        } else {
-          console.log('Geocode was not successful for the following reason: ' + status);
-          obj.viewCtrl.dismiss({place:place});
-        }
-      });
-    }
-    else
-    {
-      this.viewCtrl.dismiss({place:place});
-    }
+  // getAddressOnChange(place){
+  //   console.log(place.geometry);
+  //   var obj=this;
+  //   if(place.geometry===undefined){
+  //     this.geocoder.geocode( { 'address': this.search}, function(results, status) {
+  //       if (status == 'OK') {
+  //         results[0]['name']=obj.search;
+  //         obj.viewCtrl.dismiss({place:results[0]});
+  //       } else {
+  //         console.log('Geocode was not successful for the following reason: ' + status);
+  //         obj.viewCtrl.dismiss({place:place});
+  //       }
+  //     });
+  //   }
+  //   else
+  //   {
+  //     this.viewCtrl.dismiss({place:place});
+  //   }
+  // }
+
+  onSearchKeyPress(){
+    this.http.get(this.constants.BASE_URL_STOPS_AUTOCOMPLETE+"?query="+this.search).subscribe(data => {
+        this.items = data.json();
+        //console.log(body);
+    });
   }
 
   goBackClicked(){
